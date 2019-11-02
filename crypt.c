719ed61ed4cc6eba390a3e67d47771fbe2c69645
zachtheclimber <zachtheclimber@users.noreply.github.com>
2019-11-02 00:14:28 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ crypt.c
@@ -0,0 +1,15 @@
+// Basic Encrypter, usage ./crypt salt(2) key
+
+#include <cs50.h>
+#include <stdio.h>
+#include <crypt.h>
+
+int main (int argc, string argv[])
+{
+    
+        // Pull the first two hash characters for the salt
+        const char salt[2] = {argv[1][0], argv[1][1]};
+        char *key = argv[2];
+        char *hash = crypt(key, salt);
+        printf("%s\n", hash);
+}
