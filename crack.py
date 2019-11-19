36a75d5edbfaad42ea106a51bae0549042fd6d4d
zachtheclimber <zachtheclimber@users.noreply.github.com>
2019-11-19 00:40:17 +0000

    automated commit by submit50

--- crack.py
+++ crack.py
@@ -21,42 +21,52 @@ def main():
     for i in range(5):
         crack(i, insalt, alpha)
 
+
 def hash_check(word, insalt):
+    # Hash the inbound word
     potential = crypt.crypt(word, salt=insalt)
+    # Check it against the inputted hash
     if (potential == argv[1]):
         print(word)
         quit()
 
-def crack(length, insalt, alpha):
 
+def crack(length, insalt, alpha):
+    # Loop for 1 letter
     for letter1 in alpha:
         word = letter1
+        # Only check if this is the specified length
         if length == 1:
             hash_check(word, insalt)
 
+        # Loop for 2 letters
         if length >= 2:
             for letter2 in alpha:
                 word = letter1 + letter2
                 if length == 2:
                     hash_check(word, insalt)
 
+                # Loop for 3 letters
                 if length >= 3:
                     for letter3 in alpha:
                         word = letter1 + letter2 + letter3
                         if length == 3:
                             hash_check(word, insalt)
 
+                        # Loop for 4 letters
                         if length >= 4:
                             for letter4 in alpha:
                                 word = letter1 + letter2 + letter3 + letter4
                                 if length == 4:
                                     hash_check(word, insalt)
 
+                                # Loop for 5 letters
                                 if length >= 5:
                                     for letter5 in alpha:
                                         word = letter1 + letter2 + letter3 + letter4 + letter5
                                         if length == 5:
                                             hash_check(word, insalt)
+
 
 if __name__ == "__main__":
     main()
