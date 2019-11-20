7c60461b21600be2813f7099de143b81ecace7e9
zachtheclimber <zachtheclimber@users.noreply.github.com>
2019-11-20 19:55:11 +0000

    automated commit by check50 [check50=True]

--- helpers.py
+++ helpers.py
@@ -56,7 +56,7 @@ def splitter(s, t, n):
         split = set()
         c = 0
         for i in s:
-            if n+c < len(s):
+            if n+c <= len(s):
                 split.add(s[c:n+c])
             c += 1
 
